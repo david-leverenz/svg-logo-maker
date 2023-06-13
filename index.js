9aca422c9e808d6cdd43d7243f7049d9f148d5a9
david-leverenz <david.leverenz@gmail.com>
2023-06-13 10:03:02 -0500

    created new class based on tests

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const inquirer = require('inquirer');
 const getData = require('./lib/input.js');
 const getShapeInfo = require('./lib/shapes.js');
 const svgGenerator = require('./lib/svgString.js')
+const svgClass = require('./lib/svg.js')
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 // const htmlMaker = require('./htmlReadme.js');
+
 
 
 function init() {
     inquirer
         .prompt(getData)
         .then((data) => {
-        console.log(data);
-            fs.writeFile(('${data.shapeName}.svg'), svgGenerator({ ...data}), (err) => {
+            console.log(data);
+            const svg = new SVG();
+            svg.setText(textColor, initials);
+            svg.setShape(shapeSpecificString, shapeColor);
+            fs.writeFile(('example.svg'), svgGenerator({ ...data }), (err) => {
                 err ? console.log(err) : console.log("Success!");
             });
         });
